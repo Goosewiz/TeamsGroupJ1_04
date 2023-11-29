00cbf9e0f6da77dcc3e7d15ff6fe5bb9cda0d499
Danila <igusevdanila@gmail.com>
2023-11-29 22:35:49 +0400

    task1 update

--- src/test/gusev/java23/j1_04/task1/DatesRangeTest.java
+++ src/test/gusev/java23/j1_04/task1/DatesRangeTest.java
@@ -12,17 +12,25 @@ class DatesRangeTest {
     @Test
     void weekendCount() {
         LocalDate date1 = LocalDate.of(2023, Month.NOVEMBER, 12);
-        LocalDate date2 = LocalDate.of(2023, Month.NOVEMBER, 13);
-        LocalDate date3 = LocalDate.of(2023, Month.NOVEMBER, 14);
-        LocalDate[] answer = DatesRange.weekendCount(date1, date3);
-        assertEquals(3, answer.length);
-        assertEquals(date1, answer[0]);
-        assertEquals(date2, answer[1]);
-        assertEquals(date3, answer[2]);
-        LocalDate dateNull = null;
-        answer = DatesRange.weekendCount(date1, dateNull);
+        LocalDate date2 = LocalDate.of(2023, Month.NOVEMBER, 11);
+        LocalDate[] answer = DatesRange.weekendCount(date1, date2);
         assertEquals(0, answer.length);
-        answer = DatesRange.weekendCount(dateNull, date3);
-        assertEquals(0, answer.length);
+        date1 = LocalDate.of(2023, Month.FEBRUARY, 28);
+        date2 = LocalDate.of(2023, Month.FEBRUARY, 28);
+        answer = DatesRange.weekendCount(date1, date2);
+        assertEquals(1, answer.length);
+        assertEquals(LocalDate.of(2023,Month.FEBRUARY, 28), answer[0]);
+        date1 = LocalDate.of(2020, Month.MAY, 28);
+        date2 = LocalDate.of(2020, Month.JUNE, 02);
+        answer = DatesRange.weekendCount(date1, date2);
+        assertEquals(6, answer.length);
+        assertEquals(LocalDate.of(2020,Month.MAY, 28), answer[0]);
+        assertEquals(LocalDate.of(2020,Month.JUNE, 02), answer[answer.length-1]);
+        date1 = LocalDate.of(1901, Month.JANUARY, 01);
+        date2 = LocalDate.of(1901, Month.DECEMBER, 31);
+        answer = DatesRange.weekendCount(date1, date2);
+        assertEquals(365, answer.length);
+        assertEquals(LocalDate.of(1901, Month.JANUARY, 01), answer[0]);
+        assertEquals(LocalDate.of(1901, Month.DECEMBER, 31), answer[answer.length-1]);
     }
 }
